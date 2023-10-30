154e49c254e92d22012747887c2b7f062e35ef01
bryanbridge11 <149317049+bryanbridge11@users.noreply.github.com>
2023-10-30 19:43:24 -0400

    Add files via upload

---
+++ HW3/util.py
@@ -0,0 +1,47 @@
+import psycopg2
+from psycopg2 import Error
+
+def connect_to_db(username='raywu1990',password='test',host='127.0.0.1',port='5432',database='dvdrental'):
+	try:
+		connection = psycopg2.connect(user=username,
+					       password = password,
+					       host = host,
+					       port = port,
+					       database = database)
+		cursor = connection.cursor()
+		print("Connected to the database")
+		
+		return cursor, connection
+	
+	except (Exception, Error) as error:
+		print("Error while connecting to PostgreSQL", error)
+		
+def disconnect_from_db(connection,cursor):
+	if (connection):
+		cursor.close()
+		connection.close()
+		print("PostgreSQL connection is closed.")
+	else:
+		print("Connection does not work.")
+
+def run_and_fetch_sql(cursor, sql_string=""):
+	try:
+		cursor.execute(sql_string)
+		record = cursor.fetchall()
+		return record
+	except (Exception, Error) as error:
+		print("Errors while executing the code: ", error)
+		return -1
+		
+
+def insert_into_basket_a(cursor, sql_string=""):
+    	try:
+        	cursor.execute(sql_string)
+        	cursor.connection.commit()
+        	return True
+    	except Exception as error:
+        	cursor.connection.rollback()
+        	print(f"Error: {error}")
+        	return False
+
+
